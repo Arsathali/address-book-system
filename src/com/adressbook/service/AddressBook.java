1bc02f4c55ba7a6fa14f5bf25318f4468591582a
arsath ali <arsatharsha17@gmail.com>
2026-01-31 11:14:00 +0530

    Add UC1 ability to create contact using AddressBook

---
+++ src/com/adressbook/service/AddressBook.java
@@ -0,0 +1,12 @@
+package com.adressbook.service;
+
+import java.util.ArrayList;
+import java.util.List;
+import com.adressbook.model.ContactPerson;
+
+public class AddressBook {
+    
+    private static List<ContactPerson> addressBook = new ArrayList<>();
+
+    
+}
